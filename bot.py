d0d8f24cb7d5d6194ebb07762b47f86c8d0a43b9
KM <itmuminoff@gmail.com>
2025-01-24 16:38:20 +0500

    fx

--- bot.py
+++ bot.py
@@ -2,12 +2,12 @@ import datetime
 import random
 import asyncio
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, filters
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 
 TIME_OPTIONS = [
     "19:00", "19:30", "20:00", "20:30", "21:00", "22:00", "22:30", "23:00"
 ]
-MAX_PLAYERS = 10
+MAX_PLAYERS = 1
 
 
 def shuffle_players(players):
@@ -17,10 +17,11 @@ def shuffle_players(players):
 
 async def start(update, context):
     context.user_data['chat_id'] = update.effective_chat.id
+    context.user_data['reminder_task'] = None
     keyboard = [[InlineKeyboardButton("Сыграть", callback_data='join_game')]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        f"Саломалейкум, {update.message.from_user.username}! Хуш омади ба боти Counter-Organaizer by Комил Муминов",
+        f"Саломалейкум, {update.message.from_user.username}! Хуш омади ба боти Counter-Organizer by Комил Муминов",
         reply_markup=reply_markup)
 
 
@@ -65,20 +66,65 @@ async def set_game_time(update, context):
         + "\n".join(context.user_data['players']))
     await update.callback_query.answer()
 
+    # Подтверждение участия
     keyboard = [[
-        InlineKeyboardButton("Изменить время", callback_data='change_time')
-    ], [InlineKeyboardButton("Отменить игру", callback_data='cancel_game')]]
+        InlineKeyboardButton("Подтвердить участие", callback_data='confirm_participation')
+    ]]
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.callback_query.message.reply_text(
-        "Вы можете изменить время игры или отменить игру.",
-        reply_markup=reply_markup)
+        "Подтвердите свое участие в игре:", reply_markup=reply_markup)
+
+    # Напоминание об игре
+    await schedule_reminder(update, context)
+
+    # Уведомление всех игроков
+    for player in context.user_data['players']:
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f"{player}, игра запланирована на {context.user_data['game_start_time']}!")
+
+
+async def confirm_participation(update, context):
+    username = update.callback_query.from_user.username
+    context.user_data.setdefault('confirmed_players', []).append(username)
+
+    confirmed = len(context.user_data['confirmed_players'])
+    await update.callback_query.answer("Ваше участие подтверждено!")
+    await update.callback_query.edit_message_text(
+        f"Подтверждено игроков: {confirmed}/{MAX_PLAYERS}.\n" +
+        "\n".join(context.user_data['confirmed_players']))
+
+
+async def schedule_reminder(update, context):
+    game_time = datetime.datetime.strptime(context.user_data['game_start_time'], "%H:%M")
+    now = datetime.datetime.now()
+    game_datetime = now.replace(hour=game_time.hour, minute=game_time.minute, second=0)
+
+    # Вычисляем оставшееся время до игры
+    reminder_time = game_datetime - datetime.timedelta(minutes=15)
+    delay = (reminder_time - now).total_seconds()
+
+    if delay > 0:
+        if context.user_data['reminder_task']:
+            context.user_data['reminder_task'].cancel()
+
+        context.user_data['reminder_task'] = asyncio.create_task(remind_players(context, delay))
+    else:
+        await remind_players(context, 0)
+
+
+async def remind_players(context, delay):
+    await asyncio.sleep(delay)
+    players = context.user_data.get('players', [])
+    for player in players:
+        await context.bot.send_message(
+            chat_id=context.user_data['chat_id'],
+            text=f"{player}, напоминание! Игра начнется через 15 минут."
+        )
 
 
 async def change_game_time(update, context):
-    if context.user_data['players'][
-            0] != update.callback_query.from_user.username:
-        await update.callback_query.answer(
-            "Только лидер игры может изменить время!")
+    if context.user_data['players'][0] != update.callback_query.from_user.username:
+        await update.callback_query.answer("Только лидер игры может изменить время!")
         return
 
     keyboard = [[
@@ -91,14 +137,23 @@ async def change_game_time(update, context):
 
 
 async def cancel_game(update, context):
-    if context.user_data['players'][
-            0] != update.callback_query.from_user.username:
-        await update.callback_query.answer(
-            "Только лидер игры может отменить игру!")
+    if context.user_data['players'][0] != update.callback_query.from_user.username:
+        await update.callback_query.answer("Только лидер игры может отменить игру!")
         return
 
+    # Уведомление всех игроков
+    for player in context.user_data.get('players', []):
+        await context.bot.send_message(
+            chat_id=context.user_data['chat_id'],
+            text=f"{player}, игра была отменена."
+        )
+
+    # Очистка данных
     context.user_data['players'] = []
     context.user_data['game_start_time'] = None
+    if context.user_data.get('reminder_task'):
+        context.user_data['reminder_task'].cancel()
+
     await update.callback_query.edit_message_text(
         "Игра была отменена. Все участники удалены из списка.")
 
@@ -109,19 +164,22 @@ async def cancel_game(update, context):
         reply_markup=reply_markup)
 
 
+def log_completed_game(players, time):
+    with open("game_logs.txt", "a") as log_file:
+        log_file.write(f"Игра завершена\nВремя: {time}\nИгроки: {', '.join(players)}\n\n")
+
+
 def main():
+    proxy_url = "http://50.207.199.81:80"  # Прокси 
     application = Application.builder().token(
-        "7312402670:AAEgb72S8pIWWdsxDYK17d-nmLTB5PYxI0I").build()
+    "7312402670:AAEgb72S8pIWWdsxDYK17d-nmLTB5PYxI0I").proxy(proxy_url).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(
-        CallbackQueryHandler(join_game, pattern="join_game"))
-    application.add_handler(
-        CallbackQueryHandler(set_game_time, pattern="|".join(TIME_OPTIONS)))
-    application.add_handler(
-        CallbackQueryHandler(change_game_time, pattern="change_time"))
-    application.add_handler(
-        CallbackQueryHandler(cancel_game, pattern="cancel_game"))
+    application.add_handler(CallbackQueryHandler(join_game, pattern="join_game"))
+    application.add_handler(CallbackQueryHandler(set_game_time, pattern="|".join(TIME_OPTIONS)))
+    application.add_handler(CallbackQueryHandler(confirm_participation, pattern="confirm_participation"))
+    application.add_handler(CallbackQueryHandler(change_game_time, pattern="change_time"))
+    application.add_handler(CallbackQueryHandler(cancel_game, pattern="cancel_game"))
 
     application.run_polling()
 
